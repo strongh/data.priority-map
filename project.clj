53d2549e4865837365c66aa181c81fcb407dc9a7
Homer Strong <homer.strong@gmail.com>
2013-09-25 17:49:04 -0700

    fix project paths

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/strongh/data.priority-map"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src/main"]
-  :test-paths ["src/test"]
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"]
   :aot :all
   :dependencies [[org.clojure/clojure "1.4.0"]])
